8a8f8258e3e7274d1a90a72b2ed224b7a127e7c7
Samuel M.H <samuel.munoz@beeva.com>
2018-05-31 12:44:19 +0200

    Call with params + timer to save CSV

--- DS_annotator/webserver.py
+++ DS_annotator/webserver.py
@@ -1,7 +1,9 @@
-"""
+"""Webserver
 """
 
-import argparse
+import os
+import sys
+from threading import Timer
 
 from flask import Flask
 
@@ -10,15 +12,34 @@ from DS_annotator.server.flask import init_flask
 from DS_annotator.server.jsonrpc import init_jsonrpc
 
 
+def repeater(seconds, func):
+    print("Save annotations")
+    func()
+    Timer(seconds, repeater, args=(seconds, func) ).start()
+
+
 if __name__ == '__main__':
-    #TODO import argparse
-    # User input
-    # path_dir_imgs = sys.argv[1]
-    # path_file_out = sys.argv[2]
-    path_dir_imgs = "/home/samuelmunoz/beeva/dataset_annotator/data"
-    path_file_out = "/home/samuelmunoz/beeva/dataset_annotator/data/test1.csv"
+
+    if len(sys.argv) != 3:  # Number of arguments correct
+        print("Usage: {} <path dataset> <path annotations CSV file>".format(sys.argv[0]))
+        exit(0)
+    path_dir_imgs = sys.argv[1]
+    path_file_out = sys.argv[2]
+
+    # Dataset exists
+    if not os.path.isdir(path_dir_imgs):
+        print("ERROR: cannot find {}".format(path_dir_imgs))
+        exit(0)
+
+    print("Data directory: {}".format(os.path.abspath(path_dir_imgs)))
+    print("Annotations file: {}".format(os.path.abspath(path_file_out)))
+
+
 
     annotations = Annotations(path_dir_imgs, path_file_out)
+
+    # Save the CSV every 3 minutes.
+    repeater(3*60, annotations.save)
 
     # set the project root directory as the static folder, you can set others.
     app = Flask(__name__, static_url_path='')
